e688944f0e1ce83a422da64d976f2e444edc4c03
IvanCace <ivancace3@gmail.com>
2026-03-15 18:50:06 +0100

    Add file reading

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 #include "memory/paging/paging.h"
 #include <stddef.h>
 #include <stdint.h>
+#include "./disk/disk.h"
 
 uint16_t terminal_x = 0;
 uint16_t terminal_y = 0;
@@ -73,15 +74,11 @@ void kernel_main()
     idt_init();
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
-    char* ptr = kzalloc(4096);
-    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000,
-               (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
 
     enable_paging();
-    char* ptr2 = (char*) 0x1000;
-    ptr2[0] = 'A';
-    ptr2[1] = 'B';
-    print(ptr2);
-    print(ptr);
+
+    char buf[512];
+    disk_read_sector(0, 1, buf);
+
     enable_interupts();
 }
